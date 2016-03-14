c147b082da2346ba79a858938b856df57bd9cd23
Yi Cheng <wisechengyi@gmail.com>
2016-03-14 06:36:04 +0000

    auto reply works

--- dj/food/polls/urls.py
+++ dj/food/polls/urls.py
@@ -9,6 +9,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^verify', views.verify, name='verify'),
   url(r'^api/(?P<model>.+)', views.api, name='api'),
 ]
 
--- dj/food/polls/views.py
+++ dj/food/polls/views.py
@@ -1,9 +1,13 @@
 import json
-
+import time
+from threading import Thread
+from src.main.run import wechat
+from src.main.server import WechatData
+from pprint import pprint
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core import serializers
-
+from django.views.decorators.http import require_http_methods
 from . import models
 
 def index(request):
@@ -17,3 +21,34 @@ def api(request, model):
   print reservations
   data = serializers.serialize("json", reservations)
   return HttpResponse(data)
+
+@require_http_methods(["GET", "POST"])
+def verify(request):
+  timestamp = request.GET.get('timestamp')
+  signature = request.GET.get('signature')
+  nonce = request.GET.get('nonce')
+  pprint(request.GET)
+  pprint(request.body)
+  if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+    print "valid request"
+    echostr = request.GET.get('echostr')
+    if echostr:
+      return HttpResponse(echostr)
+    else:
+      # request.body contains the message xml
+      msg = WechatData(request.body)
+      if msg.is_text_msg():
+        t = Thread(target=send_message, args=(msg.get_from_user_name(), "hello there, fool"))
+        t.start()
+      return HttpResponse(" ")
+  else:
+    print "invalid request"
+    return HttpResponse(status=406) 
+
+def print_func(times):
+  for i in range(times):
+    print "hi there!"
+    time.sleep(1)
+
+def send_message(username, content):
+  wechat.send_text_message(username, "hello there, fool")
